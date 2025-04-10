918ab0e4a80d53977a8d5dc6a4e956f195a91480
Jose Juarez <juniojuarez@yahoo.com.br>
2025-04-10 14:34:56 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,42 @@ import { Header } from "./components/Header";
 import { InputTask } from "./components/InputTask";
 import { TaskList } from "./components/TaskList";
 
+interface ITask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
 function App() {
-  //const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function toggleTaskCompleted(taskId: string) {
+    const updateTask = tasks.map((ts) =>
+      ts.id === taskId ? { ...ts, isCompleted: !ts.isCompleted } : ts
+    );
+    setTasks(updateTask);
+  }
+  function handleCreateNewTask(task: ITask) {
+    setTasks([...tasks, task]);
+  }
+
+  function handleDeleteTask(taskId: string) {
+    const tasksWithoutDeletedOne = tasks.filter((task) => {
+      return task.id !== taskId;
+    });
+    setTasks(tasksWithoutDeletedOne);
+  }
 
   return (
     <>
       <Header />
-      <InputTask />
-      <TaskList />
+      <InputTask handleCreateNewTask={handleCreateNewTask} />
+
+      <TaskList
+        tasks={tasks}
+        toggleTaskCompleted={toggleTaskCompleted}
+        handleDeleteTask={handleDeleteTask}
+      />
     </>
   );
 }
