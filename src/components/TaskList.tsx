79ac54bfdb6b5b0582feb8f59548a13dec8024ea
Jose Juarez <juniojuarez@yahoo.com.br>
2025-04-10 14:35:47 -0300

    update archives

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,23 +1,61 @@
-import React from "react";
 import styles from "./TaskList.module.css";
-import { Tasks } from "./Tasks";
+import { Task } from "./Task";
+import { ClipboardList } from "lucide-react";
+interface ITask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
 
-export const TaskList = () => {
+interface ITaskListProps {
+  tasks: ITask[];
+  toggleTaskCompleted: (taskId: string) => void;
+  handleDeleteTask: (id: string) => void;
+}
+
+export const TaskList = ({
+  tasks,
+  toggleTaskCompleted,
+  handleDeleteTask,
+}: ITaskListProps) => {
+  const taskCount = tasks.length;
+  const isCompleted = tasks.filter((task) => task.isCompleted).length;
+
   return (
     <section className={styles.taskList}>
       <header className={styles.header}>
         <div className={styles.createdTasksContainer}>
           <span className={styles.createdTasks}>Tarefas Criadas:</span>
-          <span className={styles.createdTasksCount}>10</span>
+          <span className={styles.createdTasksCount}>{taskCount}</span>
         </div>
         <div className={styles.createdTasksContainer}>
           <span className={styles.finishTasks}>Concluidas:</span>
-          <span className={styles.createdTasksCount}>10</span>
+          <span className={styles.createdTasksCount}>{isCompleted}</span>
         </div>
       </header>
 
-      <Tasks />
-      <Tasks />
+      <div>
+        {tasks.length > 0 ? (
+          tasks.map((task) => {
+            return (
+              <Task
+                id={task.id}
+                key={task.id}
+                title={task.title}
+                isCompleted={task.isCompleted}
+                toggleTaskCompleted={toggleTaskCompleted}
+                handleDeleteTask={handleDeleteTask}
+              />
+            );
+          })
+        ) : (
+          <div className={styles.emptyTask}>
+            <ClipboardList size={56} />
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
