b05964d401eca295b516537d619d1e242f9b46e3
Jose Juarez <juniojuarez@yahoo.com.br>
2025-04-11 12:03:55 -0300

    update SRC

---
+++ src/components/InputTask.tsx
@@ -0,0 +1,61 @@
+import {
+  useState,
+  type FormEvent,
+  ChangeEvent,
+  type InvalidEvent,
+} from "react";
+import { v4 as uuidv4 } from "uuid";
+import styles from "./InputTask.module.css";
+
+interface ITask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
+interface IInputTaskProps {
+  handleCreateNewTask: (task: ITask) => void;
+}
+export const InputTask = ({ handleCreateNewTask }: IInputTaskProps) => {
+  const [readInput, setReadInput] = useState("");
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("");
+    setReadInput(event.target.value);
+  }
+
+  function handleInputNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("Esse campo não pode ficar vazio");
+  }
+
+  function createTask(event: FormEvent) {
+    event.preventDefault();
+    if (readInput === "") {
+      alert("O campo não pode ser vazio");
+    } else {
+      let taskInput: ITask = {
+        id: uuidv4(),
+        title: readInput,
+        isCompleted: false,
+      };
+      handleCreateNewTask(taskInput);
+      setReadInput("");
+    }
+  }
+
+  return (
+    <div className={styles.input_task}>
+      <form className={styles.form_input} onSubmit={createTask}>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          onInvalid={handleInputNewTaskInvalid}
+          onChange={handleNewTaskChange}
+          value={readInput}
+          required
+        />
+        <button type="submit">criar</button>
+      </form>
+    </div>
+  );
+};
---
+++ src/components/Task.tsx
@@ -0,0 +1,43 @@
+import styles from "./Task.module.css";
+import { Trash } from "lucide-react";
+
+interface ITaskProps {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+  toggleTaskCompleted: (id: string) => void;
+  handleDeleteTask: (id: string) => void;
+}
+
+export const Task = ({
+  id,
+  title,
+  isCompleted,
+  toggleTaskCompleted,
+  handleDeleteTask,
+}: ITaskProps) => {
+  function deleteTask() {
+    handleDeleteTask(id);
+  }
+  function handleChangeTask() {
+    toggleTaskCompleted(id);
+  }
+
+  return (
+    <div className={styles.task}>
+      <div
+        className={`${styles.taskItem} ${isCompleted ? styles.checked : ""}`}
+      >
+        <input
+          type="checkbox"
+          onChange={handleChangeTask}
+          checked={isCompleted}
+        />
+        <p>{title}</p>
+        <button className={styles.trashButton} onClick={deleteTask}>
+          <Trash size={16} />
+        </button>
+      </div>
+    </div>
+  );
+};
